ffc58ca2ff25260149150f4ffab3b228ace426ed
Farjad Malik <m.farjad.malik@gmail.com>
2026-02-03 01:53:05 +0500

    20260203 setup

--- src/2026/20260203/main.py
+++ src/2026/20260203/main.py
@@ -1,15 +1,15 @@
-    import polars as pl
+import polars as pl
 
-    # =============================================================================
-    # DESIGN PHILOSOPHY
-    # =============================================================================
-    # Story:
-    #
-    # Design principles:
-    # -
-    # =============================================================================
+# =============================================================================
+# DESIGN PHILOSOPHY
+# =============================================================================
+# Story:
+#
+# Design principles:
+# -
+# =============================================================================
 
-    # Load data
-    edible_plants = pl.read_csv(
-        "https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-02-03/edible_plants.csv"
-    )
+# Load data
+edible_plants = pl.read_csv(
+    "https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-02-03/edible_plants.csv"
+)
